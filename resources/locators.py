f457a21d347e52cbfdc0f6c31530d864bdbf2658
piotrlazarczyk <piotrlazarczyk@woo.network>
2024-02-07 21:53:04 +0100

    removed sample code

--- resources/locators.py
+++ resources/locators.py
@@ -13,17 +13,6 @@ class HomePageLocators:
     password_input = (By.ID, "password")
     login_containter = (By.ID, "login_containter")
     inventory_container = (By.ID, "inventory_container")
-    test_input1 = (By.XPATH, "//input[@name='password']")
-    test_input2 = (By.XPATH, "//div[@e2e-id='test']/child::input")
-    test_input3 = (By.XPATH, "//div[@class='test']/child::div")
-
-    @classmethod
-    def test_name1(cls, test: str) -> tuple:
-        return (By.XPATH, f"//div[@text()='{test}']")
-
-    @classmethod
-    def test_name2(cls, test: str) -> tuple:
-        return (By.XPATH, f"//div[@text()='{test}']")
 
 
 class InventoryPageLocators:
